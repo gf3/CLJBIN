62183a43a0e2837b35ae81a4381f391dd452870e
Gianni Chiappetta <gianni@runlevel6.org>
2012-03-05 11:41:21 -0500

    Add basic tab/bs functionality to editor textarea

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -3,11 +3,11 @@ void function() {
   $('.hidden').hide().removeClass('hidden')
 
   // Highlight Coads
-  SyntaxHighlighter.defaults['gutter'] = false
-  SyntaxHighlighter.defaults['light'] = true
-  SyntaxHighlighter.defaults['quick-code'] = false
-  SyntaxHighlighter.defaults['tab-size'] = 2
-  SyntaxHighlighter.defaults['toolbar'] = false
+  SyntaxHighlighter.defaults['gutter']      = false
+  SyntaxHighlighter.defaults['light']       = true
+  SyntaxHighlighter.defaults['quick-code']  = false
+  SyntaxHighlighter.defaults['tab-size']    = 2
+  SyntaxHighlighter.defaults['toolbar']     = false
   SyntaxHighlighter.all()
 
   // Fork
@@ -25,5 +25,23 @@ void function() {
   })
 
   // Run
+  // TODO: It
+
+  // Editor
+  $('#code').on('keydown', function(e) {
+    if (e.keyCode === 9) {
+      e.preventDefault()
+      this.value += '  '
+    }
+    else if (this.selectionStart && this.setSelectionRange && e.keyCode === 8) {
+      if (this.selectionStart >= 2) {
+        if (this.value.substr(this.selectionStart - 2, 2) === '  ') {
+          e.preventDefault()
+          this.value = this.value.substring(0, this.selectionStart - 2) + this.value.substring(this.selectionStart)
+          this.setSelectionRange(this.selectionStart, this.selectionStart)
+        }
+      }
+    }
+  }).focus()
 }()
 
